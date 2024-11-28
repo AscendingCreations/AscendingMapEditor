57dc443a430ce206a9e9cc85f607d8606a7f0f70
unknown <sherwin.salonga.new@gmail.com>
2024-11-28 20:42:26 +0800

    Updated graphics lib to 0.16.0

--- src/editor_input/preference_input.rs
+++ src/editor_input/preference_input.rs
@@ -23,24 +23,17 @@ pub fn preference_input(
     match inputtype {
         MouseInputType::LeftDown => {
             if gui.preference.keywindow.is_open {
-                let click_button =
-                    gui.preference.keywindow.click_buttons(systems, screen_pos);
+                let click_button = gui.preference.keywindow.click_buttons(systems, screen_pos);
                 if let Some(index) = click_button {
                     match index {
                         0 => gui.preference.keywindow.close_key(systems), // Cancel
                         _ => {
-                            if let Some(keycode) =
-                                &gui.preference.keywindow.key_code
-                            {
+                            if let Some(keycode) = &gui.preference.keywindow.key_code {
                                 let index = gui.preference.keywindow.key_index;
                                 config_data.key_code[index] = keycode.clone();
                                 config_data.key_code_modifier[index] =
                                     gui.preference.keywindow.key_modifier;
-                                gui.preference.update_key_list(
-                                    systems,
-                                    index,
-                                    config_data,
-                                );
+                                gui.preference.update_key_list(systems, index, config_data);
                             }
                             gui.preference.keywindow.close_key(systems)
                         } // Save
@@ -56,8 +49,7 @@ pub fn preference_input(
             }
 
             if !gui.preference.scrollbar.in_hold {
-                let click_button =
-                    gui.preference.click_buttons(systems, screen_pos);
+                let click_button = gui.preference.click_buttons(systems, screen_pos);
                 if let Some(index) = click_button {
                     match index {
                         0 => {
@@ -65,8 +57,7 @@ pub fn preference_input(
                             gui.preference.close(systems);
                         } // Cancel
                         1 => {
-                            gui.preference
-                                .reset_preference(systems, config_data);
+                            gui.preference.reset_preference(systems, config_data);
                         } // Reset
                         _ => {
                             config_data.save_config().unwrap();
@@ -95,53 +86,37 @@ pub fn preference_input(
                 }
 
                 if gui.preference.select_menu_button(systems, screen_pos) {
-                    open_preference_tab(
-                        &mut gui.preference,
-                        systems,
-                        config_data,
-                    );
+                    open_preference_tab(&mut gui.preference, systems, config_data);
                 }
 
                 match gui.preference.selected_menu {
                     PREF_TAB_GENERAL => {
-                        if let Some(index) = gui.preference.is_coloreditor_open
-                        {
-                            if gui
-                                .preference
-                                .in_color_selection(systems, screen_pos)
-                            {
+                        if let Some(index) = gui.preference.is_coloreditor_open {
+                            if gui.preference.in_color_selection(systems, screen_pos) {
                                 gui.preference.select_text(systems, screen_pos);
-                                if gui.preference.click_color_selection_button(
-                                    systems, screen_pos,
-                                ) {
-                                    if let SettingData::ColorSelection(
-                                        colorselection,
-                                    ) =
+                                if gui
+                                    .preference
+                                    .click_color_selection_button(systems, screen_pos)
+                                {
+                                    if let SettingData::ColorSelection(colorselection) =
                                         &mut gui.preference.setting_data[index]
                                     {
-                                        let data =
-                                            colorselection.color_editor.data;
+                                        let data = colorselection.color_editor.data;
                                         systems.gfx.set_color(
                                             colorselection.image,
-                                            Color::rgba(
-                                                data[0], data[1], data[2],
-                                                data[3],
-                                            ),
+                                            Color::rgba(data[0], data[1], data[2], data[3]),
                                         );
                                         match index {
                                             GENERAL_MAPCOLOR => {
-                                                config_data
-                                                    .map_selection_color = data
+                                                config_data.map_selection_color = data
                                             }
                                             GENERAL_TILECOLOR => {
-                                                config_data
-                                                    .tile_selection_color = data
+                                                config_data.tile_selection_color = data
                                             }
                                             _ => {}
                                         }
                                     }
-                                    gui.preference
-                                        .hide_color_selection(systems);
+                                    gui.preference.hide_color_selection(systems);
                                 }
                                 return;
                             }
@@ -150,8 +125,7 @@ pub fn preference_input(
                         if let Some(config_index) =
                             gui.preference.select_config(systems, screen_pos)
                         {
-                            match &mut gui.preference.setting_data[config_index]
-                            {
+                            match &mut gui.preference.setting_data[config_index] {
                                 SettingData::Checkbox(checkbox) => {
                                     if checkbox.is_select {
                                         checkbox.set_select(systems, false);
@@ -160,47 +134,34 @@ pub fn preference_input(
                                     }
                                     match config_index {
                                         GENERAL_HIDEFPS => {
-                                            config_data.hide_fps =
-                                                checkbox.is_select;
+                                            config_data.hide_fps = checkbox.is_select;
                                             systems.gfx.set_visible(
                                                 gui.labels[LABEL_FPS],
                                                 !checkbox.is_select,
                                             );
                                         }
                                         GENERAL_HIDETILEBG => {
-                                            config_data.hide_tileset_bg =
-                                                checkbox.is_select;
-                                            systems.gfx.set_visible(
-                                                gui.bg_layout[2],
-                                                !checkbox.is_select,
-                                            );
+                                            config_data.hide_tileset_bg = checkbox.is_select;
+                                            systems
+                                                .gfx
+                                                .set_visible(gui.bg_layout[2], !checkbox.is_select);
                                         }
                                         GENERAL_HIDEMAPBG => {
-                                            config_data.hide_mapview_bg =
-                                                checkbox.is_select;
-                                            systems.gfx.set_visible(
-                                                gui.bg_layout[1],
-                                                !checkbox.is_select,
-                                            );
+                                            config_data.hide_mapview_bg = checkbox.is_select;
+                                            systems
+                                                .gfx
+                                                .set_visible(gui.bg_layout[1], !checkbox.is_select);
                                         }
                                         _ => {}
                                     }
-                                    gui.preference
-                                        .hide_color_selection(systems);
+                                    gui.preference.hide_color_selection(systems);
                                 }
                                 SettingData::ColorSelection(colorselection) => {
-                                    if gui
-                                        .preference
-                                        .is_coloreditor_open
-                                        .is_none()
-                                    {
-                                        colorselection
-                                            .open_color_editor(systems);
-                                        gui.preference.is_coloreditor_open =
-                                            Some(config_index);
+                                    if gui.preference.is_coloreditor_open.is_none() {
+                                        colorselection.open_color_editor(systems);
+                                        gui.preference.is_coloreditor_open = Some(config_index);
                                     } else {
-                                        gui.preference
-                                            .hide_color_selection(systems);
+                                        gui.preference.hide_color_selection(systems);
                                     }
                                 }
                                 _ => {}
@@ -210,12 +171,9 @@ pub fn preference_input(
                         }
                     }
                     PREF_TAB_KEYBIND => {
-                        if let Some(key_index) =
-                            gui.preference.select_keylist(systems, screen_pos)
+                        if let Some(key_index) = gui.preference.select_keylist(systems, screen_pos)
                         {
-                            gui.preference
-                                .keywindow
-                                .open_key(systems, key_index);
+                            gui.preference.keywindow.open_key(systems, key_index);
                         }
                     }
                     _ => {}
@@ -223,11 +181,9 @@ pub fn preference_input(
             }
         }
         MouseInputType::LeftDownMove => {
-            gui.preference.scrollbar.move_scrollbar(
-                systems,
-                screen_pos.y,
-                false,
-            );
+            gui.preference
+                .scrollbar
+                .move_scrollbar(systems, screen_pos.y, false);
             if gui
                 .preference
                 .update_scroll(gui.preference.scrollbar.cur_value)
@@ -251,11 +207,7 @@ pub fn preference_input(
     }
 }
 
-pub fn preference_key_input(
-    systems: &mut DrawSetting,
-    event: &KeyEvent,
-    gui: &mut Interface,
-) {
+pub fn preference_key_input(systems: &mut DrawSetting, event: &KeyEvent, gui: &mut Interface) {
     if !event.state.is_pressed() {
         return;
     }
@@ -272,18 +224,17 @@ pub fn preference_key_input(
                     &mut gui.preference.setting_data[index]
                 {
                     if colorselection.color_editor.is_open {
-                        colorselection.color_editor.textbox
-                            [gui.preference.editing_index]
+                        colorselection.color_editor.textbox[gui.preference.editing_index]
                             .enter_numeric(systems, event, 3, false);
 
                         let value = colorselection.color_editor.textbox
                             [gui.preference.editing_index]
                             .data
                             .parse::<i64>()
-                            .unwrap_or_default();
-                        colorselection.color_editor.data
-                            [gui.preference.editing_index] =
-                            (value as u8).min(255);
+                            .unwrap_or_default()
+                            .min(255);
+                        colorselection.color_editor.data[gui.preference.editing_index] =
+                            value as u8;
                     }
                 }
             }
